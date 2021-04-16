c518c325582f9a4e17d2b2cd3d18fcc8d55d8cd3
Ana Arine <analucia.arine@gmail.com>
2021-04-16 14:18:48 -0300

    Add Greetings files

---
+++ Greetings.kt
@@ -0,0 +1,17 @@
+
+fun main(args: Array<String>) {
+    println("Hello, ${args[0]}")  //Run -> Edit Configurations
+
+val isUnit = println("This is an expression")
+    println(isUnit)
+
+    val temperature = 10
+    val isHot = if (temperature > 40) true else false
+    println(isHot)
+
+    val message = "The fish is ${ if (temperature > 40) "fried" else "safe" }."
+    println(message)
+
+
+
+}
---
+++ Greetings2.kt
@@ -0,0 +1,8 @@
+
+fun main(args: Array<String>) {
+
+  //  val hour = 22
+    val isEarly = println({args[5].toInt()})
+        //"Good morning, Kotlin." else "Good night, Kotlin.")
+
+}
