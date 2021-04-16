6d61e59edb84ec74c394a9fd54d92a1388237761
Ana Arine <analucia.arine@gmail.com>
2021-04-16 18:10:57 -0300

    Add fishFood function

--- AMS.kt
+++ AMS.kt
@@ -8,11 +8,27 @@ fun main(args: Array<String>) {
 
 fun feedTheFish() {
     val day = randomDay()
-    val food = "pellets"
+    val food = fishFood(day)
     println("Today is $day and the fish are going to eat $food.")
 }
 
 fun randomDay() : String {
     val week = listOf("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
     return week[Random().nextInt(7)]  //conta de 0 a 6. 7 é o bound e será excluído.
+}
+
+fun fishFood (day : String) : String {
+
+   return when (day) {
+        "Monday" -> "flakes"
+        "Tuesday" -> "pellets"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Saturday" -> "lettuce"
+        "Sunday" -> "plankton"
+       else -> "spirulina"
+    }
+
+
 }
